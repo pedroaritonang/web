06fbc5c01b9d757e0f331a2c9d6f3ce271cc746b
Pedro Agusto <pedroagusto727@gmail.com>
2026-04-24 09:22:33 +0700

    Upload Github pertamaku

---
+++ data.js
@@ -0,0 +1,11 @@
+const databaseProyek = {
+    "sistem_pos": {
+        judul: "Sistem pos",
+        keahlian: ["JavaScript","PHP"],
+        gambar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkwKQTqlYbq4noIHSkb33t7SkA1v29kgnOMw&s",
+        latar: "Ini adalah proyek magang dimana saya membuat sistem POS untuk PT ROSCOE",
+        tantangan: "Kebutuhan dari ptnya sangat rumit",
+        pelajaran: "Saya mendapatkan pelajaran bahwa sistem pos PT Roscoe ideal skala kecil",
+        link: ""
+    },
+};
